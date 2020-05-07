5bc88e97e706a8ea02ee60571fb28d2bf44657a1
Thaksina <thaksina.j@ku.th>
2020-05-07 12:42:01 +0700

    All done

--- PizzaShop/src/Pizza.java
+++ PizzaShop/src/Pizza.java
@@ -1,3 +1,46 @@
-public class Pizza {
+public abstract class Pizza {
+
+    final void makePizza(){
+
+        if(customerWantNeapolitan()){
+            addNeapolitanDough();
+        }
+
+        if(customerWantRoman()){
+            addRomanDough();
+        }
+
+
+        if(customerWantMeat()){
+            addMeat();
+        }
+
+        putInBox();
+    }
+
+
+
+    abstract void addMeat();
+    abstract void  addNeapolitanDough();
+    abstract void addRomanDough();
+
+
+    boolean customerWantNeapolitan(){
+        return true;
+    }
+
+    boolean customerWantRoman(){
+        return true;
+    }
+
+
+    boolean customerWantMeat(){
+        return true;
+    }
+
+    public void putInBox(){
+        System.out.print("Put the Pizza in box");
+    }
+
 
 }
